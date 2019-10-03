fea06e09091f8c4d9ed36239ca5f443ae0be7e85
Lars Koopmans <larsk2009@live.nl>
2019-10-03 14:33:34 +0200

    Added comandline arguments for main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import tissue_scattering as sim
+import sys, getopt
 
-data = sim.scatter(750, 0.1, 0.3, 0.05, 0.05, 1, 3, 0.150, 0.9)
+opts, args = getopt.getopt(sys.argv[1:], "w", "wavelength=")
+wavelength = 660
+
+for opt, arg in opts:
+	if opt == "--wavelength":
+		wavelength = arg
+
+data = sim.scatter(int(wavelength), 0.1, 0.3, 0.05, 0.05, 1, 3, 0.150, 0.9)
 print(data)
+
+print(sim.calcAbsorptionCoefficient(940))
